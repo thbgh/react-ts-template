3751d985b9431d03c4d63b293ad705de3d24253c
thb <thbwork@163.com>
2022-05-10 13:58:23 +0800

    引入Eslint 和 Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import AntdComp from "./components/AntdComp";
-import "./App.css";
-import logo from "./logo.svg";
+import React, { useState } from 'react';
+import AntdComp from './components/AntdComp';
+import './App.css';
+import logo from './logo.svg';
 
 const App = () => (
-	<div className="App">
-		<div className="App-header">
-			<img src={logo} className="App-logo" />
-		</div>
-		<AntdComp />
-	</div>
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+    <AntdComp />
+  </div>
 );
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,89 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-	//模式 development/production  //在package.json的script中可以配置
-	// mode: "development",
-	//打包入口文件
-	entry: { main: "./src/index.tsx" },
-	// 打包的出口文件和路径
-	output: {
-		path: path.join(__dirname, "./dist"),
-		filename: "mian.[contenthash:8].js",
-		clean: true,
-	},
-	devServer: {
-		//开发服务器的配置
-		port: 1024, //端口
-		open: true, //打包完成自动打开浏览器
-		compress: true, //启用压缩
-	},
-	module: {
-		rules: [
-			// 处理jsx
-			{
-				test: /\.(j|t)sx?$/,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				options: {
-					babelrc: false,
-					presets: [
-						require.resolve("@babel/preset-react"),
-						[
-							"@babel/preset-typescript",
-							{
-								isTSX: true,
-								allExtensions: true,
-							},
-						],
-					],
-				},
-			},
-			// 处理css和less
-			{
-				test: /\.css|less$/,
-				exclude: /node_modules/,
-				use: ["style-loader", "css-loader", "less-loader"],
-			},
-			// 处理字体、图片等文件
-			{
-				test: /\.ttf|woff|woff2|svg|png|jpg|git$/,
-				use: "url-loader",
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./public/index.html", // 使用的模板html，没有指定的话会自动生成index.html。所以可以不需要public文件夹下的index.html
-			// filename: "index.html", // 打包生成的html文件名，默认为index
-			title: "WebpackPro",
-		}),
-		new BundleAnalyzerPlugin(), // 使用默认配置
-		// 默认配置的具体配置项
-		// new BundleAnalyzerPlugin({
-		//   analyzerMode: 'server',
-		//   analyzerHost: '127.0.0.1',
-		//   analyzerPort: '8888',
-		//   reportFilename: 'report.html',
-		//   defaultSizes: 'parsed',
-		//   openAnalyzer: true,
-		//   generateStatsFile: false,
-		//   statsFilename: 'stats.json',
-		//   statsOptions: null,
-		//   excludeAssets: null,
-		//   logLevel: info
-		// })
-	],
-	resolve: {
-		//省略后缀名
-		extensions: ["*", ".js", ".jsx", ".ts", ".tsx", ".json", ".svg"],
-		// 配置路径别名
-		alias: {
-			"@assets": path.resolve("./src/assets"),
-			"@views": path.resolve("./src/views"),
-			"@utils": path.resolve("./src/utils"),
-			"@components": path.resolve("./src/components"),
-		},
-	},
+  //模式 development/production  //在package.json的script中可以配置
+  // mode: "development",
+  //打包入口文件
+  entry: { main: './src/index.tsx' },
+  // 打包的出口文件和路径
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'mian.[contenthash:8].js',
+    clean: true,
+  },
+  devServer: {
+    //开发服务器的配置
+    port: 1024, //端口
+    open: true, //打包完成自动打开浏览器
+    compress: true, //启用压缩
+  },
+  module: {
+    rules: [
+      // 处理jsx
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          babelrc: false,
+          presets: [
+            require.resolve('@babel/preset-react'),
+            [
+              '@babel/preset-typescript',
+              {
+                isTSX: true,
+                allExtensions: true,
+              },
+            ],
+          ],
+        },
+      },
+      // 处理css和less
+      {
+        test: /\.css|less$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      // 处理字体、图片等文件
+      {
+        test: /\.ttf|woff|woff2|svg|png|jpg|git$/,
+        use: 'url-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // 使用的模板html，没有指定的话会自动生成index.html。所以可以不需要public文件夹下的index.html
+      // filename: "index.html", // 打包生成的html文件名，默认为index
+      title: 'WebpackPro',
+    }),
+    new BundleAnalyzerPlugin(), // 使用默认配置
+    // 默认配置的具体配置项
+    // new BundleAnalyzerPlugin({
+    //   analyzerMode: 'server',
+    //   analyzerHost: '127.0.0.1',
+    //   analyzerPort: '8888',
+    //   reportFilename: 'report.html',
+    //   defaultSizes: 'parsed',
+    //   openAnalyzer: true,
+    //   generateStatsFile: false,
+    //   statsFilename: 'stats.json',
+    //   statsOptions: null,
+    //   excludeAssets: null,
+    //   logLevel: info
+    // })
+  ],
+  resolve: {
+    //省略后缀名
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.json', '.svg'],
+    // 配置路径别名
+    alias: {
+      '@assets': path.resolve('./src/assets'),
+      '@views': path.resolve('./src/views'),
+      '@utils': path.resolve('./src/utils'),
+      '@components': path.resolve('./src/components'),
+    },
+  },
 };
