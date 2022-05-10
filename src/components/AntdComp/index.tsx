d3302a409602f0890e94add85207d0b5065e39ee
thb <thbwork@163.com>
2022-05-10 13:28:29 +0800

    add styled-components

--- src/components/AntdComp/index.tsx
+++ src/components/AntdComp/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Modal } from "antd";
-import "./index.less";
+import styled from "styled-components";
 
 export default function () {
 	const [isModalVisible, setIsModalVisible] = useState(false);
@@ -22,16 +22,28 @@ export default function () {
 		return name + ": " + age;
 	};
 	return (
-		<div className="antdComp">
-			<h3>这是一个antd组件的demo</h3>
-			<Button type="primary" onClick={showModal}>
-				Open Modal
-			</Button>
-			<Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
-			</Modal>
-		</div>
+		<Wrapper>
+			<div className="antdComp">
+				<h3>这是一个antd组件的demo</h3>
+				<Button type="primary" onClick={showModal}>
+					Open Modal
+				</Button>
+				<Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+					<p>Some contents...</p>
+				</Modal>
+			</div>
+		</Wrapper>
 	);
 }
+
+const Wrapper = styled.div`
+	.antdComp {
+		// background-color: red;
+		text-align: center;
+		h3 {
+			color: red;
+		}
+	}
+`;
